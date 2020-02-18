0bd86d8f52bb22ff1e5852683b3c507ae4225155
Robert Stepanov <robertdstepanov@gmail.com>
2020-02-18 17:18:36 -0500

    MVP

---
+++ cars/carsRouter.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const db = require('../data/dbConfig');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  db("cars")
+    .then(cars => {
+      res.json(cars);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to retrieve cars" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("cars")
+    .where({ id })
+    .first()
+    .then(cars => {
+      res.json(cars);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to retrieve cars" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const carData = req.body;
+  db("cars")
+    .insert(carData)
+    .then(ids => {
+      db("cars")
+        .where({ id: ids[0] })
+        .then(newCarEntry => {
+          res.status(201).json(newCarEntry);
+        });
+    })
+    .catch(err => {
+      console.log("POST error", err);
+      res.status(500).json({ message: "Failed to store new car" });
+    });
+});
+
+module.exports = router;
