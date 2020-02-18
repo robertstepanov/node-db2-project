070702693172067d56269094c082d0015c15f995
Robert Stepanov <robertdstepanov@gmail.com>
2020-02-18 17:45:55 -0500

    MVP and Stretch

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require('../data/dbConfig');
+const db = require("../data/dbConfig");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -40,6 +40,36 @@ router.post("/", (req, res) => {
     .catch(err => {
       console.log("POST error", err);
       res.status(500).json({ message: "Failed to store new car" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  db("cars")
+    .where({ id })
+    .update(changes)
+    .update(changes)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "couldnt update car" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  db("cars")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "couldnt delete car" });
     });
 });
 
